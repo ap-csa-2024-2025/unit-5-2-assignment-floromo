f519fb0e33a1044b1ed9226daaf7f6bdd630acf4
floromo <flormmmp@gmail.com>
2024-09-19 10:02:10 -0700

    completed assignment

--- Clock.java
+++ Clock.java
@@ -1,10 +1,30 @@
 public class Clock
 {
+
   public static void main(String[] args)
-  {
-    // test code here
+    {
+      timeOfDay(12);
+    }
+
+  public static void timeOfDay (int x){
+    String day;
+
+    if (x==0 || x==24) {
+      day = "midnight";
+    } else if (x==12) {
+      day = "noon";
+    } else if (x==18) {
+      day = "dusk";
+    } else if (x > 0 && x < 12) {
+      day = "morning";
+    } else if (x > 12 && x < 18) {
+      day = "afternoon";
+    } else if (x > 18 && x < 24) {
+      day = "evening";
+    } else {
+      day = "try again";
+    }
+
+    System.out.print(day);
   }
-
-  // write code for timeOfDay here
-
 }
--- Coins.java
+++ Coins.java
@@ -2,9 +2,21 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    coinConverter(1234);
   }
 
-  // write code for coinConverter
+  public static void coinConverter(int x){
+    int dollars = x/100;
+    int quarters = (x%100)/25;
+    int dimes = ((x%100)-(quarters*25))/10;
+    int nickles = ((x%100)-(quarters*25)-(dimes*10))/5;
+    int pennies = (x%100)-(quarters*25)-(dimes*10)-(nickles*5);
+
+    System.out.println( "Dollar Bills: " + dollars + "\n" + 
+                        "Quarters: " + quarters + "\n" + 
+                        "Dimes: " + dimes + "\n" + 
+                        "Nickles: " + nickles + "\n" + 
+                        "Pennies: " + pennies );
+  }
 
 }
